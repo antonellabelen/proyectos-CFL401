51330575860d98278c60cbe1768b45ee9a913660
Alumno <Alumno@CONECTARIGUALDAD>
2019-07-03 21:24:56 -0300

    Le agregamos una clase de mascotas, un iterator y una clase de persona.

---
+++ Sabatella-test- ArrayList/src/sabatella/test/arraylist/Mascotas.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sabatella.test.arraylist;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Mascotas {
+
+    static void add(Mascotas perro1) {
+    }
+    private String nombre;
+    private int patas;
+    private int edad;
+
+    public Mascotas(String nombre, int patas, int edad) {
+        this.nombre = nombre;
+        this.patas = patas;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
--- Sabatella-test- ArrayList/src/sabatella/test/arraylist/SabatellaTestArrayList.java
+++ Sabatella-test- ArrayList/src/sabatella/test/arraylist/SabatellaTestArrayList.java
@@ -6,6 +6,7 @@
 package sabatella.test.arraylist;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class SabatellaTestArrayList {
@@ -14,14 +15,14 @@ public class SabatellaTestArrayList {
 
  
     public static void main(String[] args) {
-       System.out.println("Hola");
+      /* System.out.println("Hola");
        Scanner teclado=new Scanner(System.in);
        ArrayList elem= new ArrayList();
        ArrayList <Integer> elem2=new ArrayList();
-      int dato=teclado.nextInt();
+     int dato=teclado.nextInt();
        elem.add("perro");
        elem.add(4);
-       elem.add("edad");
+      elem.add("edad");
        System.out.println(elem);
       /*while(dato>=0) {
           elem2.add(dato);
@@ -34,10 +35,53 @@ public class SabatellaTestArrayList {
        } catch(Exception e) {
            System.out.println(e.toString());*/
        
-        ArrayList mascotas=new ArrayList();
-        mascotas.add("pichulo");
-        mascotas.add(4);
-        mascotas.add(17);
+      
+        ArrayList <Mascotas> mascotas =new ArrayList();
+        mascotas.add(new Mascotas("pichulo",4,13));
+        System.out.println(mascotas);
+        Mascotas gato1=new Mascotas("mishi",4,10);
+        Mascotas loro1=new Mascotas("paco",2,25);
+        mascotas.add(new Mascotas("mishi",4,10));
+        mascotas.add(new Mascotas("paco",2,25));
+        System.out.println(mascotas);
+        Scanner entrada= new Scanner(System.in);
+        String nombre;
+        System.out.println("ingrese el nombre de su mascota y si desea salir,escriba salir");
+        nombre=entrada.nextLine();
+        int patas,edad;
+      // while(!nombre.equals("salir")){
+      while(!nombre.toUpperCase().equals("salir".toUpperCase())){
+             patas=entrada.nextInt();
+             edad=entrada.nextInt();
+             System.out.println(new Mascotas(nombre,patas,edad));
+             System.out.println("ingrese el nombre de su mascota y si desea salir,escriba salir");
+             entrada.nextLine();
+             nombre=entrada.nextLine();
+        }
+            //System.out.println(mascotas);
+            Iterator it=mascotas.iterator();
+            //it.hasNext()
+            while(it.hasNext()){
+                Mascotas mascota=(Mascotas) it.next();
+               //System.out.println(((Mascotas) it.next()).getNombre());
+               System.out.println(mascota.getNombre() + mascota.getPatas() + mascota.getEdad());
+        
+        
+        
+        
+
+         
+          }
+          
+        
+              
+          
+            
+        }
+       
+        
+        
+        
         
            
        }
@@ -47,7 +91,7 @@ public class SabatellaTestArrayList {
        
     
        
-    }
+    
 
            
        
